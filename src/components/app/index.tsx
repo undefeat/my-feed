7f401504a1848f362ca891cdc62b65388a9a5e3a
Dmytro Kostiuk <undefeat.eridan@gmail.com>
2019-02-03 14:07:05 +0200

    Initial setup

---
+++ src/components/app/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>I am set up!</h1>
+                <p>Lorem ipsum...</p>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import 'src/style.css';
+
+import App from 'src/components/app';
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
